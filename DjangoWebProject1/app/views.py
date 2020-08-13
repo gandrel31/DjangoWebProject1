ac785dbce348ead185f42b71227eeb9e6ee40e16
gabri <gabri@PC-GAB>
2020-08-13 14:10:58 +0200

    Premier essai

---
+++ DjangoWebProject1/app/views.py
@@ -0,0 +1,62 @@
+"""
+Definition of views.
+"""
+
+from datetime import datetime
+from django.http import HttpRequest
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from .forms import CustomUserCreationForm
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def register(request):
+    if request.method == 'POST':
+        f = CustomUserCreationForm(request.POST)
+        if f.is_valid():
+            f.save()
+            messages.success(request, 'Account created successfully')
+            return redirect(home)
+
+    else:
+        f = CustomUserCreationForm()
+
+    return render(request, 'app/register.html', {'form': f})
